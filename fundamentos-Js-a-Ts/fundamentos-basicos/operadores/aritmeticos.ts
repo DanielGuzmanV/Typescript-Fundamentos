95ddb6ee4e47349be187cd17751c0a51ab169e76
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-04-10 10:46:18 -0400

    aritmeticos -fundamentos basicos-

---
+++ fundamentos-Js-a-Ts/fundamentos-basicos/operadores/aritmeticos.ts
@@ -0,0 +1,23 @@
+let number1: number = 10;
+let number2: number = 7;
+
+// Tipos de operadores aritmeticos:
+console.log('Suma o adicion:', number1 + number2); // suma
+console.log('Resta:', number1 - number2); // Resta
+console.log('Multiplicacion:', number1 * number2); // Multiplicacion
+console.log('Division:', number1 / number2); // DIvision
+console.log('Modulo:', number1 % number2); // Modulo (resto de una division)
+console.log('Potencia:', number1 ** number2); // Exponente
+
+// Formas de incrementar:
+console.log(++number1);
+console.log(number1++);
+console.log('Resultado final', number1);
+
+// Formas de decrementar:
+console.log(--number2);
+console.log(number2--);
+console.log('Resultado final', number2);
+
+
+
