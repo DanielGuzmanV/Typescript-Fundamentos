5f486a57aed70f863166b72c4931e02caa0e183a
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-04-10 11:16:22 -0400

    asignaciones -funciones basicas-

---
+++ fundamentos-Js-a-Ts/fundamentos-basicos/operadores/asignaciones.ts
@@ -0,0 +1,52 @@
+//  Asignacion al mismo valor:
+// Ejemplo de suma:
+let num1: number = 10;
+num1 = num1 + 10;
+num1 += 5;
+console.log('Resultado final suma:', num1);
+console.log('===============================');
+
+// Ejemplo resta:
+let num2: number = 40;
+num2 = num2 - 15;
+num2 -= 5;
+console.log('Resultado final resta:', num2);
+console.log('===============================');
+
+// Ejemplo multiplicacion:
+let num3: number = 5;
+num3 = num3 * 10;
+num3 *= 3;
+console.log('Resultado final multiplicacion:', num3);
+console.log('===============================');
+
+// Ejemplo division:
+let num4: number = 20;
+num4 = num4 / 2;
+num4 /= 2;
+console.log('Resultado final division:', num4);
+console.log('===============================');
+
+// Ejemplo modulo:
+let num5: number = 55;
+num5 = num5 % 2;
+num5 %= 2;
+console.log('Resultado final modulo:', num5);
+console.log('===============================');
+
+// Ejemplo potencia:
+let num6: number = 20;
+num6 = num6 ** 2;
+num6 **= 2;
+console.log('Resultado final potencia:', num6);
+console.log('===============================');
+
+
+
+
+
+
+
+
+
+
