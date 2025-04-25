e4fcf6831f0746ace4d3230b17fff380dab93f76
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-04-24 23:36:30 -0400

    manejo-DOM-7 finalizado -fundamentos intermedios-

---
+++ fundamentos-Js-a-Ts/fundamentos-intermedios/manipulacion-dom/manejo-DOM-7/script.ts
@@ -0,0 +1,59 @@
+// EJEMPLO 1:
+// Cambiar el contenido dinamicamente
+const contentDiv = document.getElementById('contenedorDiv');
+
+// Sera vera como texto sin formato:
+document.getElementById('botonTexto')?.addEventListener('click', function() {
+    contentDiv!.textContent = '<i>Este es solo un texto sin formato</i>';
+})
+
+// Se vera como un tetxto con formato en cursiva:
+document.getElementById('botonHTML')?.addEventListener('click', () => {
+    contentDiv!.innerHTML = '<i>Esto es HTML intrepreado</i>'
+})
+
+
+// ==================================================================
+// ==================================================================
+
+
+// EJEMPLO 2:
+// Prevenir ataques XSS usando "textContent"
+document.getElementById('mostrarTexto')?.addEventListener('click', () => {
+    const labelInputValue = document.getElementById('entrada') as HTMLInputElement | null;
+    
+    if(labelInputValue){
+        // Evitamos la inyeccion de codigo malicioso
+        const resultado = document.getElementById('resultadoDiv');
+        if(resultado) {
+            resultado.textContent = labelInputValue.value;
+        }
+    }
+})
+
+
+// ==================================================================
+// ==================================================================
+
+
+// EJEMPLO 3:
+// Cargar contenido dinamico en una lista:
+document.getElementById('agregarElementos')?.addEventListener('click', () => {
+    const nuevosElementos: string = '<li>Nuevo elemento</li>'
+
+    const ulElements = document.getElementById('listaElement') as HTMLElement | null;
+    
+    if(ulElements) {
+        // Si usamos innerHTML se agregaran multiples elementos HTML
+        ulElements.innerHTML += nuevosElementos
+
+        // Si usamos textContent se agregaran textos sin formato
+        // ulElements.textContent += nuevosElementos;
+    }
+
+})
+
+
+
+
+
